7f5ae4b8a491692c767c305e29d0d1970d6c2e69
BrookeAL0012 <bbcecil17@gmail.com>
2022-07-06 15:41:25 -0500

    initial message

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+
+
+
+        public bool IsAPalindrome(string word)
+
+        {
+
+            var reversedWord = word.Reverse();
+
+            return word.ToLower().SequenceEqual(reversedWord);
+        }
+    }
+}
