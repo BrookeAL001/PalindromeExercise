881239ebaa486522aeb4f21089e849088fa17d4b
BrookeAL0012 <bbcecil17@gmail.com>
2022-07-11 11:46:11 -0500

    second edit

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,26 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
-namespace PalindromeExerciseTests
+namespace Palindrome.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("elephant", false)]
+        [InlineData("kayak", true)]
+        [InlineData("rotator", true)]
+        [InlineData("tree", false)]
+        
+        public void Test1(string word, bool expected)
         {
-
+            //arrange
+            var tester = new WordSmith();
+            //act
+            var actual = tester.IsAPalindrome(word);
+            //assert
+            Assert.Equal(actual, expected);
         }
     }
 }
